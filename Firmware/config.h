9ea4170c6d80964b2b32904ce4eb6ecedac984d0
Aviral <senbp15@gmail.com>
2026-07-25 22:48:06 +0530

    Add files via upload

---
+++ Firmware/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x0001
+#define DEVICE_VER      0x0001
+
+#define MANUFACTURER    YourName
+#define PRODUCT         INDIKEY
+
+#define DEBOUNCE 5
